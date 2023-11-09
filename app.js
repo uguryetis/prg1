324791a3c7176bdb613b23e36c7fe203503803ef
uguryetis <yetisugur42@gmail.com>
2023-11-09 14:24:34 +0300

    added mysql-connection

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express=require('express')
+const db_connect=require('./db/mysql_connect')
 const { send } = require('express/lib/response')
 const app =express()
-app.get('/app',function (req,res){
-    res.send('hello world')
+app.get('/',function (req,res){
+    res.send('Anasayfa')
+})
+app.get('/app',function(req,res){
+    res.send('app sayfası')
+
 })
 app.listen(3000)
